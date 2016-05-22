f986801f5d79ca9824d9bc2adbbab64bdede4b80
malokuS <malokuselim@gmail.com>
2016-05-22 19:35:03 +0200

    Reset file

---
+++ MoveGreenApp/Pages/Layouts/Layout_WiFi.xaml.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace MoveGreenApp
+{
+	public partial class Layout_WiFi : ContentPage
+	{
+		public Layout_WiFi ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
